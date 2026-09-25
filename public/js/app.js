96ecb68dab5f16745a303412dbfb9d97eddbd092
ThanatosVz1 <viveksinghyadav.1xz@gmail.com>
2026-09-25 22:45:24 +0530

    fix: remove star below navbar button, keep admin terminal input text boxes completely blank

--- public/js/app.js
+++ public/js/app.js
@@ -153,8 +153,6 @@ class HermesApp {
 
     if (this.currentUser) {
       if (authBtn) authBtn.classList.add('hidden');
-      const _navStar = document.getElementById('navbar-secret-star');
-      if (_navStar) _navStar.classList.add('hidden');
       if (userBadge) {
         userBadge.classList.remove('hidden');
         userBadge.innerHTML = `
